de2ded5d3ecf7ec010fdaeca171bb8afac1154db
EC2 Default User <ec2-user@ip-172-31-11-206.ap-northeast-1.compute.internal>
2023-03-01 05:16:53 +0000

    [middle]task3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,13 @@ class Book < ApplicationRecord
 
   has_one_attached :profile_image
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
